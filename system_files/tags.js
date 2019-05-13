514c1cf9b8954532d3fe4b34eac63702250a6a85
Tony-Nagga <44039699+Tony-Nagga@users.noreply.github.com>
2019-05-13 20:09:37 +0300

    Update tags.js

--- system_files/tags.js
+++ system_files/tags.js
@@ -2,32 +2,32 @@ const Discord = require('discord.js');
 
 exports.get = () => {
     tags = ({
-		"FBI": "Federal Bureau of Investigation",
-	    	"LSPD": "Сотрудник LSPD",
-		"SFPD": "Сотрудник SFPD",
-		"RCSD": "Сотрудник RCSD",
-		"LVPD": "Сотрудник LVPD",
+	"FBI": "Federal Bureau of Investigation",
+	"LSPD": "Сотрудник LSPD",
+	"SFPD": "Сотрудник SFPD",
+	"RCSD": "Сотрудник RCSD",
+	"LVPD": "Сотрудник LVPD",
 
-		"LSa": "Военнослужащий LS",
-		"SFa": "Военнослужащий SF",
-		"JLV": "Надзиратель Jail LV",
+	"LSa": "Военнослужащий LS",
+	"SFa": "Военнослужащий SF",
+	"JLV": "Надзиратель Jail LV",
 
-		"MCLS": "Мед. работник MCLS",
-		"MCSF": "Мед. работник MCSF",
-		"MCLV": "Мед. работник MCLV",
+	"MCLS": "Мед. работник MCLS",
+	"MCSF": "Мед. работник MCSF",
+	"MCLV": "Мед. работник MCLV",
 
-		"CNN LS": "Сотрудник радиоцентра LS",
-		"CNN SF": "Сотрудник радиоцентра SF",
-		"CNN LV": "Сотрудник радиоцентра LV",
+	"CNN LS": "Сотрудник радиоцентра LS",
+	"CNN SF": "Сотрудник радиоцентра SF",
+	"CNN LV": "Сотрудник радиоцентра LV",
 
-		"CBLS": "Сотрудник Банка LS",
-		"DS": "Сотрудник Автошколы",
-		"GOV": "Сотрудник Правительствa",
+	"CBLS": "Сотрудник Банка LS",
+	"DS": "Сотрудник Автошколы",
+	"GOV": "Сотрудник Правительствa",
 
-		"Yakuza": "Якудза",
-		"WMC": "Warlock MC",
-		"LCN": "La Cosa Nostra",
-		"RM": "Russian Mafia",
+	"Yakuza": "Якудза",
+	"WMC": "Warlock MC",
+	"LCN": "La Cosa Nostra",
+	"RM": "Russian Mafia",
     });
     return tags;
 };
