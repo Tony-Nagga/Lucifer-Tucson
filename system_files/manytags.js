76e89d22d2b2cbf427849001a4aaaa8e1586f9f7
Tony-Nagga <44039699+Tony-Nagga@users.noreply.github.com>
2019-05-13 20:12:59 +0300

    Update manytags.js

--- system_files/manytags.js
+++ system_files/manytags.js
@@ -2,27 +2,32 @@ const Discord = require('discord.js');
 
 exports.get = () => {
     let manytags = [
-        	"FBI",
-	    	"LSPD",
-		"SFPD",
-		"RCSD",
-		"LVPD",
-		"LSa",
-		"SFa",
-		"JLV",
-		"MCLS",
-		"MCSF",
-		"MCLV",	
-		"CNN LS",
-		"CNN SF",
-		"CNN LV",	
-		"CBLS",
-		"DS",
-		"GOV",
-		"Yakuza",
-		"WMC",
-		"LCN",
-		"RM",
+        "FBI",
+	"LSPD",
+	"SFPD",
+	"RCSD",
+	"LVPD",
+
+	"LSa",
+	"SFa",
+	"JLV",
+
+	"MCLS",
+	"MCSF",
+	"MCLV",	
+
+	"CNN LS",
+	"CNN SF",
+	"CNN LV",
+
+	"CBLS",
+	"DS",
+	"GOV",
+
+	"Yakuza",
+	"WMC",
+	"LCN",
+	"RM"
     ];
     return manytags;
 }
