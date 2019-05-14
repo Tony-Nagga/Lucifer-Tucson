f934592c6873760665c2a5a91eecefb8e51accb7
Tony-Nagga <44039699+Tony-Nagga@users.noreply.github.com>
2019-05-14 19:30:17 +0300

    Update code.js

--- code.js
+++ code.js
@@ -56,7 +56,7 @@ async function get_support(){
 
 function timer(){ 
     setInterval(() => { 
-    let server = bot.guilds.get("438803520288981004"); 
+    let server = bot.guilds.get(serverid); 
     let channel = server.channels.find(c => c.name == "general");
     channel.send("Привет, я Артем!"); 
     }, 1000); // 1 секунда = 1000
